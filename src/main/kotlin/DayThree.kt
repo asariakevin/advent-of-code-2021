f2380b4a8ca8df7dce3375e962ebac47c415d414
Kevin Asaria <asariakevin@gmail.com>
2021-12-03 10:06:39 +0300

    Day 3 Binary Diagnostics Part 2

--- src/main/kotlin/DayThree.kt
+++ src/main/kotlin/DayThree.kt
@@ -5,38 +5,101 @@ fun main(){
     val diagnosticReport = File("/home/asaria/Desktop/Hie/src/day_three_input_file.txt").readLines()
     val lengthOfBinaryString = diagnosticReport.first().length
 
-    var gammaRateBinaryString = ""
-    var epsilonRateBinaryString = ""
+    var oxygenGeneratorDiagnosticReport = diagnosticReport
+    var co2ScrubberDiagnosticReport = diagnosticReport
 
-    var gammaRateInteger = 0
-    var epsilonRateInteger = 0
-    var powerConsumptionInteger = 0
+    for( index in 0 until lengthOfBinaryString){
 
-    for( index in 0..(lengthOfBinaryString - 1)){
-        var numberOf0Bits = 0
-        var numberOf1Bits = 0
-
-        diagnosticReport.forEach { binaryString ->
-
-            if (binaryString[index].digitToInt(2) == 0){
-                numberOf0Bits++
-            }else{
-                numberOf1Bits++
+            val mostCommonValueInCurrentPosition = determineMostOrLeastCommonValue(index,oxygenGeneratorDiagnosticReport,"MOST")
+            when(mostCommonValueInCurrentPosition){
+                0 -> {
+                   oxygenGeneratorDiagnosticReport = oxygenGeneratorDiagnosticReport.filter { it[index].digitToInt(2) == 0  }
+                }
+                1 -> {
+                    oxygenGeneratorDiagnosticReport = oxygenGeneratorDiagnosticReport.filter { it[index].digitToInt(2) == 1  }
+                }
+                2 -> {
+                    oxygenGeneratorDiagnosticReport = oxygenGeneratorDiagnosticReport.filter { it[index].digitToInt(2) == 1  }
+                }
+                4 -> println("Something went wrong")
             }
+        if (oxygenGeneratorDiagnosticReport.size == 1){
+            break
         }
 
-        if (numberOf0Bits > numberOf1Bits){
-            gammaRateBinaryString+="0"
-            epsilonRateBinaryString+="1"
+
+    }
+
+    for( index in 0 until lengthOfBinaryString){
+
+        val leastCommonValueInCurrentPosition = determineMostOrLeastCommonValue(index,co2ScrubberDiagnosticReport,"LEAST")
+        when(leastCommonValueInCurrentPosition){
+            0 -> {
+                co2ScrubberDiagnosticReport = co2ScrubberDiagnosticReport.filter { it[index].digitToInt(2) == 0  }
+            }
+            1 -> {
+                co2ScrubberDiagnosticReport = co2ScrubberDiagnosticReport.filter { it[index].digitToInt(2) == 1  }
+            }
+            2 -> {
+                co2ScrubberDiagnosticReport = co2ScrubberDiagnosticReport.filter { it[index].digitToInt(2) == 0  }
+            }
+            4 -> println("Something went wrong")
+        }
+        if (co2ScrubberDiagnosticReport.size == 1){
+            break
+        }
+
+
+    }
+
+    val lifeSupportRating = oxygenGeneratorDiagnosticReport.first().toInt(2) * co2ScrubberDiagnosticReport.first().toInt(2)
+
+    println("Life support rating: $lifeSupportRating")
+
+}
+
+/**
+ * Determine the most common value in the current
+ * bit position
+ *
+ * return 0 if 0 most common , 1 if 1 most common, 2 if commonly equal
+ *
+ * @param option can be either Most or Least
+ * **/
+fun determineMostOrLeastCommonValue( bitPosition: Int , diagnosticRepostList: List<String>, option : String): Int {
+
+    var numberOf0Bits = 0
+    var numberOf1Bits = 0
+
+    diagnosticRepostList.forEach { binaryString ->
+
+        if (binaryString[bitPosition].digitToInt(2) == 0){
+            numberOf0Bits++
         }else{
-            gammaRateBinaryString+="1"
-            epsilonRateBinaryString+="0"
+            numberOf1Bits++
         }
     }
 
-    gammaRateInteger = gammaRateBinaryString.toInt(2)
-    epsilonRateInteger = epsilonRateBinaryString.toInt(2)
-    powerConsumptionInteger = gammaRateInteger * epsilonRateInteger
+    return when(option){
+        "MOST" ->{
+            if (numberOf0Bits > numberOf1Bits){
+                 0
+            }else if ( numberOf1Bits > numberOf0Bits){
+                 1
+            }else{
+                 2
+            }
+        }
+        "LEAST" -> {
+            if (numberOf0Bits < numberOf1Bits){
+                 0
+            }else if ( numberOf1Bits < numberOf0Bits){
+                 1
+            }else{
+                 2
+            }
+        }
+        else -> 4
+    }
 
-    println("Power Consumption: $powerConsumptionInteger")
 }
